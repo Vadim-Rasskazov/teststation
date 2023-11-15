e5dd3393999f90c8afab52955a0d7cf1ca735f9b
Vadim <vadim.rasskazow@gmail.com>
2023-11-15 13:54:39 +0300

    0.3.0 Java 21 update
    Signed-off-by: Vadim <vadim.rasskazow@gmail.com>

--- java.sh
+++ java.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt install -y openjdk-19-jdk
+sudo apt install -y openjdk-21-jdk
 sudo apt install maven
 wget https://dlcdn.apache.org/maven/maven-4/4.0.0-alpha-8/binaries/apache-maven-4.0.0-alpha-8-bin.tar.gz
 sudo tar xzf apache-maven-4.0.0-alpha-8-bin.tar.gz -C /opt/
@@ -7,7 +7,7 @@ sudo tar xzf apache-maven-4.0.0-alpha-8-bin.tar.gz -C /opt/
 #sudo rm -R /opt/apache-maven-3.8.7
 sudo rm /opt/maven
 sudo ln -s /opt/apache-maven-4.0.0-alpha-8 /opt/maven
-export JAVA_HOME=/usr/lib/jvm/java-19-openjdk-amd64
+export JAVA_HOME=/usr/lib/jvm/java-21-openjdk-amd64
 export M2_HOME=/opt/maven
 export MAVEN_HOME=/opt/maven
 export PATH=${M2_HOME}/bin:${PATH}
